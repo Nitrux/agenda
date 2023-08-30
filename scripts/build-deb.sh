60279b39002f1c4ccf41c07acfccec71f3049e87
UriHerrera <hello@uriherrera.com>
2023-08-30 15:30:58 -0600

    Update build-deb.sh

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -66,7 +66,7 @@ checkinstall -D -y \
 	--pakdir=. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=agenda \
-	--requires="akonadi-server,libc6,libkf5akonadicore5,libkf5akonadicontact5,libkf5calendarsupport5,libkf5coreaddons5,libkf5eventviews5,libkf5i18n5,libkf5kiocore5,libqt5core5a,libqt5gui5,libqt5qml5,libqt5sql5,libqt5widgets5,mauikit-calendar-git \(\>= 3.0.1+git\),mauikit-git \(\>= 3.0.1+git\)" \
+	--requires="akonadi-server,libc6,libkf5akonadicore5,libkf5akonadicontact5,libkf5calendarsupport5,libkf5coreaddons5,libkf5eventviews5,libkf5i18n5,libkf5kiocore5,libmariadb3,libqt5core5a,libqt5gui5,libqt5qml5,libqt5sql5,libqt5widgets5,mariadb-common,mauikit-calendar-git \(\>= 3.0.1+git\),mauikit-git \(\>= 3.0.1+git\)" \
 	--nodoc \
 	--strip=no \
 	--stripso=yes \
